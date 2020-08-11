41d9b400f86f95a0c631ae0b6d6bcd9cb13c1448
mordak <mordak@mordak-pc.debian>
2020-08-11 19:31:12 -0400

    Typescript boilerplate

---
+++ src/HLSVideoReader.ts
@@ -0,0 +1,7 @@
+class HLSVideoReader {
+    constructor() {}
+    start(): string {
+        var output: string = "HLSVideoReader started";
+        return output;
+    }
+}
---
+++ src/build.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# previous i dit tsc --init to generate a dummy tscconfig.json file
+tsc
+exit 0
+
