068fef52694c129169674fbbebbcf685848b9bdd
mordak <mordak@mordak-pc.debian>
2020-08-11 17:35:50 -0400

    Set playlist.m3u8 as an universal filename

--- ressources/generate.sh
+++ ressources/generate.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 mkdir $1
-ffmpeg -hide_banner -y -i $1.mp4 -c:v copy -c:a copy -f hls -hls_list_size 0 -hls_time 10 -hls_segment_type fmp4 -hls_segment_filename $1/original_%03d.m4s $1/original_playlist.m3u8
+ffmpeg -hide_banner -y -i $1.mp4 -c:v copy -c:a copy -f hls -hls_list_size 0 -hls_time 10 -hls_segment_type fmp4 -hls_segment_filename $1/original_%03d.m4s $1/playlist.m3u8
 exit $?
