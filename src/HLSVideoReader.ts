8b330aba1e11ed6e931c4ab3fc3bfa6aac802b0d
mordak <mordak@mordak-pc.debian>
2020-08-11 20:05:03 -0400

    Just reading a simple video with HTML5 player

--- src/HLSVideoReader.ts
+++ src/HLSVideoReader.ts
@@ -1,6 +1,8 @@
 class HLSVideoReader {
     constructor() {}
-    start(): string {
+    init(video :HTMLVideoElement): string {
+        console.log(video);
+        video.src = "http://localhost/farador.mp4";
         var output: string = "HLSVideoReader started";
         return output;
     }
