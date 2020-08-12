596c954d542d898068f9c012fc68936a212f2399
mordak <mordak@mordak-pc.debian>
2020-08-12 09:58:58 -0400

    Async parse playlst with Promesses

--- web/js/HLSVideoReader.ts
+++ web/js/HLSVideoReader.ts
@@ -8,28 +8,12 @@ export default class HLSVideoReader {
         var output: string = "HLSVideoReader started";
         return output;
     }
-    setUrl(url: string) {
-        var manifest = [
-            '#EXTM3U',
-            '#EXT-X-VERSION:3',
-            '#EXT-X-TARGETDURATION:6',
-            '#EXT-X-MEDIA-SEQUENCE:0',
-            '#EXT-X-DISCONTINUITY-SEQUENCE:0',
-            '#EXTINF:6,',
-            '0.ts',
-            '#EXTINF:6,',
-            '1.ts',
-            '#EXTINF:6,',
-            '2.ts',
-            '#EXT-X-ENDLIST'
-        ].join('\n');
-
+    addPlaylist(raw_text: string) {
         // Any is necessary for bullshit reasons
         var parser = new (Parser as any)();
-        parser.push(manifest);
+        parser.push(raw_text);
         parser.end();
         let parsedManifest = parser.manifest;
         console.log(parsedManifest);
-        console.log(url);
     }
 }
--- web/js/main.js
+++ web/js/main.js
@@ -1,10 +1,42 @@
 import HLSVideoReader from './build/HLSVideoReader.js';
-//import { Parser } from './m3u8-parser.es.js';
-
 const baseUrl = 'http://localhost/';
+
+function getTxtAsync(url) {
+    // Promises require two functions: one for success, one for failure
+    return new Promise(function (resolve, reject) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.onload = () => {
+            if (xhr.status === 200) {
+                // We can resolve the promise
+                resolve(xhr.response);
+            } else {
+                // It's a failure, so let's reject the promise
+                reject("Unable to load RSS at :" + url);
+            }
+        }
+        xhr.onerror = () => {
+            // It's a failure, so let's reject the promise
+            reject("Unable to load RSS at :" + url);
+        };
+        xhr.send();
+    });
+}
+
 function start() {
     var reader = new HLSVideoReader();
     console.log(reader.init(document.getElementsByTagName("video")[0]));
-    reader.setUrl(baseUrl + 'ts/farador/');
+
+    Promise.all([getTxtAsync(baseUrl + 'ts/farador/playlist.m3u8'), getTxtAsync(baseUrl + 'ts/melanchon/playlist.m3u8')]).then(values => {
+        console.log(values.length)
+        values.forEach(function(elem) {
+            console.log(elem);
+            reader.addPlaylist(elem);
+        });
+        console.log("end of loading playlist");
+    }, reason => {
+        console.log(reason)
+    });
 }
+
 start();
