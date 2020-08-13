37133b9bda66de0474a049463d291da44621bb71
mordak <mordak@mordak-pc.debian>
2020-08-13 18:11:24 -0400

    USe a promesse to break the hell of multiple callbacks + errors managing

--- web/js/main.js
+++ web/js/main.js
@@ -41,6 +41,76 @@ function getBinaryAsync(url) {
     });
 }
 
+// Attempts to call a given function 'fn' with smaller and smaller data slice. (1, 1/2, 1/4, 1/8...)
+function loadVideo(sourceBuffer, arrayBuffer) {
+    return new Promise(function (resolve, reject) {
+        console.info("data length: " + arrayBuffer.byteLength);
+        try {
+            let data = new Uint8Array(arrayBuffer);
+            let divisor = 1;
+            let start_offset = 0;
+            let chunk_size = data.length;
+
+            let append_chunk = function() {
+                let chunk = data.subarray(start_offset, ((start_offset + chunk_size) > data.length) ? data.length : start_offset + chunk_size);
+                sourceBuffer.appendBuffer(chunk);
+            }
+
+            let onupdate = function() {
+                start_offset += chunk_size;
+                if (start_offset < data.length) {
+                    try {
+                        append_chunk();
+                    } catch (e) {
+                        throw (e);
+                    }
+                } else {
+                    // Consider here that the loading was succesfull
+                    removeAllEventListeners();
+                    resolve();
+                }
+            }
+            let onerror = function() {
+                console.error(e);
+                throw("error evt: " + e);
+            }
+            let onabort = function() {
+                console.error(a);
+                throw("abort evt: " + a);
+            }
+            let removeAllEventListeners = function() {
+                sourceBuffer.removeEventListener('updateend', onupdate);
+                sourceBuffer.removeEventListener('error', onerror);
+                sourceBuffer.removeEventListener('abort', onabort);
+            }
+
+            sourceBuffer.addEventListener('updateend', onupdate);
+            sourceBuffer.addEventListener('error', onerror);
+            sourceBuffer.addEventListener('abort', onabort);
+
+            (function appendFragments() {
+                try {
+                    // WARN: We consider that when the first append is succesfull, the others will be same
+                    append_chunk();
+                } catch (e) {
+                    divisor *= 2;
+                    chunk_size = Math.floor(data.length / divisor);
+                    if (Math.floor(data.length / divisor) == 0) {
+                        // In this critical case, even one byte per one byte causes an error
+                        throw("Too many reduction: " + e);
+                    } else {
+                        appendFragments();
+                    }
+                }
+            })();
+        }
+        catch (e) {
+            removeAllEventListeners();
+            reject("Unexpected error: " + e);
+        }
+    });
+}
+
 function start() {
     var reader = new HLSVideoReader();
     var promesses = new Array;
@@ -61,8 +131,8 @@ function start() {
     console.log(MediaSource.isTypeSupported(mimeCodec));
     var video = document.getElementsByTagName("video")[0];
     video.src = URL.createObjectURL(mediaSource);
-    mediaSource.addEventListener('sourceopen', sourceOpen);
 
+    mediaSource.addEventListener('sourceopen', sourceOpen);
     function sourceOpen (_) {
         var mediaSource = this;
         console.log(mediaSource.readyState);
@@ -70,52 +140,12 @@ function start() {
         //sourceBuffer.appendWindowEnd = 4.0;
 
         getBinaryAsync('http://localhost/farador_dashinit.mp4').then(arrayBuffer => {
-            console.log("data length: " + arrayBuffer.byteLength);
-
-            // Attempts to call a given function 'fn' with smaller and smaller data slice. (1, 1/2, 1/4, 1/8...)
-            {
-            let data = new Uint8Array(arrayBuffer);
-            let divisor = 1;
-            let start_offset = 0;
-            let chunk_size;
-
-            let append_chunk = function() {
-                let chunk = data.subarray(start_offset, ((start_offset + chunk_size) > data.length) ? data.length : start_offset + chunk_size);
-                sourceBuffer.appendBuffer(chunk);
-            }
-
-            sourceBuffer.addEventListener('updateend', function (_) {
-                start_offset += chunk_size;
-                if (start_offset < data.length) {
-                    append_chunk();
-                } else {
-                    // Consider here that the loading was succesfull
-                    mediaSource.endOfStream();
-                }
+            loadVideo(sourceBuffer, arrayBuffer).then(_ => {
+                mediaSource.endOfStream();
+            },
+            reason => {
+                console.error(reason);
             });
-            sourceBuffer.addEventListener('error', function (e) {
-                console.error(e);
-            });
-            sourceBuffer.addEventListener('abort', function (a) {
-                console.error(a);
-            });
-
-            (function appendFragments() {
-                try {
-                    chunk_size = Math.floor(data.length / divisor);
-                    // WARN: We consider that when the first append is succesfull, the others will be same
-                    append_chunk();
-                } catch (e) {
-                    divisor *= 2;
-                    if (Math.floor(data.length / divisor) == 0) {
-                        // In this critical case, even one byte per one byte causes an error
-                        throw e;
-                    } else {
-                        appendFragments();
-                    }
-                }
-            })();
-            }
         }, reason => {
             console.log(reason)
         });
